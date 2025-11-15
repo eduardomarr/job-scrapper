01e15af3a240eb10d5421775b1d81bca53beb0ef
Eduardo Marques Nepomuceno <eduardo.marques@platformbuilders.io>
2025-11-15 15:27:10 -0300

    add some sites

--- src/config/jobSites.ts
+++ src/config/jobSites.ts
@@ -3,33 +3,44 @@ import { JobSite, SearchConfig } from "../types";
 // Configuração de sites e palavras-chave para monitorar
 export const jobSites: JobSite[] = [
   {
-    name: 'Visa/Pismo',
-    url: 'https://careers.smartrecruiters.com/Visa/careers-at-pismo',
-    type: 'smartrecruiters'
+    name: "Visa/Pismo",
+    url: "https://careers.smartrecruiters.com/Visa/careers-at-pismo",
+    type: "smartrecruiters",
   },
-  // Adicione mais sites aqui
-  // {
-  //   name: 'Outra Empresa',
-  //   url: 'https://...',
-  //   type: 'custom'
-  // }
+  {
+    name: "Quinto andar",
+    url: "https://carreiras.quintoandar.com.br/vagas-abertas/",
+    type: "custom",
+  },
+  {
+    name: "Ifood",
+    url: "https://carreiras.ifood.com.br/jobs/",
+    type: "smartrecruiters",
+  },
+  {
+    name: "Nubank",
+    url: "https://picpay.com/oportunidades-de-emprego-e-carreiras/central-de-vagas",
+    type: "smartrecruiters",
+  },
 ];
 
 export const keywords: string[] = [
-  'frontend',
-  'front-end',
-  'react',
-  'javascript',
-  'typescript',
-  'fullstack',
-  'full-stack',
-  'nodejs',
-  'node.js',
-  'reactss'
+  "frontend",
+  "front-end",
+  "react",
+  "javascript",
+  "typescript",
+  "fullstack",
+  "full-stack",
+  "nodejs",
+  "node.js",
+  "reactjs",
+  "react.js",
+  "staff",
 ];
 
 // Configurações de busca
 export const searchConfig: SearchConfig = {
   caseSensitive: false,
-  matchWholeWord: false
+  matchWholeWord: false,
 };
